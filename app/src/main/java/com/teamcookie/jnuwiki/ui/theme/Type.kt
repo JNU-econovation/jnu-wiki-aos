7815d1db8ff1a0ff6add84ba275ebf0f10bea69b
K_Gs:D <rootachieve6053@gmail.com>
2023-09-27 17:37:46 +0900

    feat : font

--- app/src/main/java/com/teamcookie/jnuwiki/ui/theme/Type.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/ui/theme/Type.kt
@@ -1,15 +1,27 @@
-package com.teamcooki.jnuwiki.ui.theme
+package com.teamcookie.jnuwiki.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.teamcookie.jnuwiki.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_thin, FontWeight.Thin, FontStyle.Normal),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold, FontStyle.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -32,3 +44,4 @@ val Typography = Typography(
     )
     */
 )
+
