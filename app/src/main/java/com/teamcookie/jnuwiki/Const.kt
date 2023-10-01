ff5adebe823a2f9ca434e21a1fe7cf8f2032a031
K_Gs:D <rootachieve6053@gmail.com>
2023-10-02 02:35:01 +0900

    feat : 루트명 상수

--- app/src/main/java/com/teamcookie/jnuwiki/Const.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/Const.kt
@@ -1,6 +1,8 @@
 package com.teamcookie.jnuwiki
 
 object Const {
+    const val MAPS = "MAPS"
+    const val LOGIN = "LOGIN"
     const val HOME = "HOME"
     const val CREATE = "CREATE"
     const val MY = "MY"
