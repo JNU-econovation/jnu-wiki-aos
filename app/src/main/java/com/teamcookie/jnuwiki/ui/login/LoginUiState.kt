2861c91615ca36fa4b399b6e4b5994a03b867ad3
K_Gs:D <rootachieve6053@gmail.com>
2023-10-08 01:57:36 +0900

    feat : login ui state에 상태 추가

--- app/src/main/java/com/teamcookie/jnuwiki/ui/login/LoginUiState.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/ui/login/LoginUiState.kt
@@ -3,4 +3,6 @@ package com.teamcookie.jnuwiki.ui.login
 data class LoginUiState(
     val id : String = "",
     val pw : String = "",
+    val message : String = "",
+    val isLogin : Boolean = false
 )
