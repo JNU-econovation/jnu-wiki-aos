baa9e94cb7ef8ee459106ece19c7f27595b18400
K_Gs:D <rootachieve6053@gmail.com>
2023-10-09 21:44:56 +0900

    fix : info dto nullable

--- app/src/main/java/com/teamcookie/jnuwiki/model/dto/ResponseUserInfoDTO.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/model/dto/ResponseUserInfoDTO.kt
@@ -2,12 +2,17 @@ package com.teamcookie.jnuwiki.model.dto
 
 data class ResponseUserInfoDTO(
     val success: Boolean,
-    val response: UserInfo,
-    val error: ErrorDTO
+    val response: UserInfo?,
+    val error: ErrorDTO?
 )
 
 data class UserInfo(
     val id: Int,
-    val nickname: String,
+    val nickName: String,
     val password: String
+)
+
+data class ResultInfo(
+    val name: String,
+    val result: Int
 )
