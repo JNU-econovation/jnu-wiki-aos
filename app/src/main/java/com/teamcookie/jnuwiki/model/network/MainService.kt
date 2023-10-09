dc197ac51b9bf5f39e6974d74ad2b824a6b70174
K_Gs:D <rootachieve6053@gmail.com>
2023-10-09 21:44:04 +0900

    feat : 토큰 재발급 관련 service 추가

--- app/src/main/java/com/teamcookie/jnuwiki/model/network/MainService.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/model/network/MainService.kt
@@ -3,7 +3,9 @@ package com.teamcookie.jnuwiki.model.network
 
 import com.teamcookie.jnuwiki.model.dto.RequestLoginDTO
 import com.teamcookie.jnuwiki.model.dto.ResponseLoginDTO
+import com.teamcookie.jnuwiki.model.dto.ResponseTokenDTO
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -11,5 +13,6 @@ interface MainService {
     @POST("/members/login")
     fun checkLogin(@Body request: RequestLoginDTO): Call<ResponseLoginDTO>
 
-
+    @POST("/members/refresh-token")
+    suspend fun issueToken() : Response<ResponseTokenDTO>
 }
