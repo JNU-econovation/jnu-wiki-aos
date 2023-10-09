5e21d5075afb65c92bb6c948ac32549e00885e34
K_Gs:D <rootachieve6053@gmail.com>
2023-10-09 21:44:24 +0900

    fix : info service를 suspend로

--- app/src/main/java/com/teamcookie/jnuwiki/model/network/AuthService.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/model/network/AuthService.kt
@@ -2,9 +2,11 @@ package com.teamcookie.jnuwiki.model.network
 
 import com.teamcookie.jnuwiki.model.dto.ResponseUserInfoDTO
 import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface AuthService {
-    @POST("/members/info")
-    fun getUserInfo() : Call<ResponseUserInfoDTO>
+    @GET("/members/info")
+    suspend fun getUserInfo() : Response<ResponseUserInfoDTO>
 }
