4991945ab6c08ed624b25e204a6ab7736e309286
K_Gs:D <rootachieve6053@gmail.com>
2023-10-11 03:33:26 +0900

    feat : check email viewmodel

--- app/src/main/java/com/teamcookie/jnuwiki/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/teamcookie/jnuwiki/ui/signin/SignInViewModel.kt
@@ -1,13 +1,18 @@
 package com.teamcookie.jnuwiki.ui.signin
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.teamcookie.jnuwiki.model.dto.RequestCheckEmailDTO
+import com.teamcookie.jnuwiki.model.dto.RequestCheckNicknameDTO
+import com.teamcookie.jnuwiki.model.repository.MainRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class SignInViewModel : ViewModel(){
-
+    private val repository = MainRepository
     private val _uiState = MutableStateFlow(SignInUiState())
     val uiState: StateFlow<SignInUiState> = _uiState.asStateFlow()
 
@@ -31,4 +36,26 @@ class SignInViewModel : ViewModel(){
     fun submit(){
         
     }
+
+    fun checkEmail(){
+        viewModelScope.launch {
+            val result = repository.checkEmail(RequestCheckEmailDTO(uiState.value.email))
+            result.onFailure {Throw ->
+                _uiState.update {
+                    it.copy(message = Throw.message?: "Error")
+                }
+            }.onSuccess {info ->
+                if(info.result == 200){
+                    _uiState.update {
+                        it.copy(isSignIn = true, message = "사용가능한 이메일입니다.")
+                    }
+                }else{
+                    _uiState.update {
+                        it.copy(message = it.message)
+                    }
+                }
+            }
+        }
+    }
+
 }
